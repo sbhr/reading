08f0e4794a5e2bf37ee158f26294fc4418d85f99
sbhr <test@test.com>
2018-04-10 23:43:11 +0900

    二重インクルード防止

---
+++ robe-c++/Redef1.cpp
@@ -0,0 +1,8 @@
+#include "Redef1.h"
+#include "Redef1.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << N << endl;
+}
---
+++ robe-c++/Redef1.h
@@ -0,0 +1,6 @@
+#ifndef REDEF1_H_20180410
+#define REDEF1_H_20180410
+
+const int N = 10;
+
+#endif
